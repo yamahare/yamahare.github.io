dce92df80f697d4a6086d668b91a0d410a13d6b7
yamahare <jidai.norio@gmail.com>
2018-12-30 15:12:28 +0900

    [add]imageminのjpegとpng用pluginを追加

--- tasks/images.js
+++ tasks/images.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp')
 const imagemin = require('gulp-imagemin')
+const imageminJpegtran = require('imagemin-jpegtran');
 const { paths } = require('./config')
 
 // ファイルの圧縮
 function images() {
   return gulp.src(paths.images.src)
-    .pipe(imagemin())
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+    ]))
     .pipe(gulp.dest(paths.images.dest))
 }
 
