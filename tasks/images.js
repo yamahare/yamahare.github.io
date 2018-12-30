7e76fc66ba407affbb587a0209ad4d77bb677f1e
yamahare <jidai.norio@gmail.com>
2018-12-30 15:18:46 +0900

    [add]imageminのsvgも追加

--- tasks/images.js
+++ tasks/images.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const imagemin = require('gulp-imagemin')
 const imageminJpegtran = require('imagemin-jpegtran');
+const imageminSvgo = require('imagemin-svgo');
 const { paths } = require('./config')
 
 // ファイルの圧縮
@@ -9,6 +10,12 @@ function images() {
     .pipe(imagemin([
       imagemin.jpegtran({progressive: true}),
       imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
     ]))
     .pipe(gulp.dest(paths.images.dest))
 }
